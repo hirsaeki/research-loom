208e20a4f78381cbd997709adb28edaa78b14d29
hirsaeki <5356955+hirsaeki@users.noreply.github.com>
2026-09-08 23:37:51 +0900

    Complete Writer section-input review boundaries

--- plugins/local_application/__init__.py
+++ plugins/local_application/__init__.py
@@ -4,6 +4,7 @@ from .application import LocalResearchApplication, LocalStaticAuthorizationProvi
 from .facade import LocalApplicationError
 from .project_input_facade import LocalApplicationFacade
 from .workspace import LocalWorkspace, LocalWorkspaceError, OpenedLocalWorkspace
+from .writer_composition_service import verify_section_input_root as verify_writer_section_input
 
 __all__ = [
     "LocalApplicationError",
@@ -13,4 +14,5 @@ __all__ = [
     "LocalWorkspace",
     "LocalWorkspaceError",
     "OpenedLocalWorkspace",
+    "verify_writer_section_input",
 ]
